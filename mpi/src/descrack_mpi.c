2469bc9cf9f843b47ca558a42927be762c6ddd77
skansion <12028343+skansion@users.noreply.github.com>
2020-10-08 16:30:38 +0200

    Fix versione MPI

--- mpi/src/descrack_mpi.c
+++ mpi/src/descrack_mpi.c
@@ -33,33 +33,31 @@ int main(int argc, char **argv) {
      * 
      * Calcola tutte le potenze da 0 a 8 (compreso) della dimensione dell'alfabeto
      * Se il processo ha rank diverso da "size - 1":
-     *   Preparati alla ricezione di un bcast asincrono dal processo con rank "size - 1":
-     *     Per ogni valore della lunghezza della chiave e finchè la chiave non è stata trovata:
-     *       Imposta i valori dell'intervallo su cui iterare in base a size, rank, valori possibili e dimensione alfabeto
-     *       Per ogni valore nell'intervallo e finchè la chiave non è stata trovata:
-     *         Controlla se il bcast è arrivato
-     *         Se il bcast non è ancora arrivato:
-     *           Ottieni la chiave relativa al valore
-     *           Testa la chiave
-     *           Se la chiave è stata trovata:
-     *             Manda un messaggio asincrono al processo con rank "size - 1" e aspetta la ricezione
-     *         Altrimenti:
-     *           Setta il flag "chiave trovata"
-     *       Attendi la fine degli altri processi
+     *   Per ogni valore della lunghezza della chiave e finchè la chiave non è stata trovata:
+     *     Imposta i valori dell'intervallo su cui iterare in base a size, rank, valori possibili e dimensione alfabeto
+     *     Per ogni valore nell'intervallo e finchè la chiave non è stata trovata:
+     *       Ottieni la chiave relativa al valore
+     *       Testa la chiave
+     *     Se la chiave è stata trovata in questo processo:
+     *       Invia un messaggio asincrono al processo con rank "size - 1" e aspetta la ricezione
+     *     Attendi l'invio del bcast da parte del processo con rank "size -1"
+     *     Leggi il bcast e setta il rispettivo flag
+     *   Attendi la fine degli altri processi
      * Altrimenti:
      *   Preparati alla ricezione di un messaggio asincrono da un qualsiasi processo
      *     Per ogni valore della lunghezza della chiave e finchè la chiave non è stata trovata:
      *       Imposta i valori dell'intervallo su cui iterare in base a size, rank, valori possibili e dimensione alfabeto
      *       Per ogni valore nell'intervallo e finchè la chiave non è stata trovata:
-     *         Controlla se il messaggio è arrivato
-     *         Se il messaggio non è ancora arrivato:
-     *           Ottieni la chiave relativa al valore
-     *           Testa la chiave
-     *           Se la chiave è stata trovata:
-     *             Manda un bcast asincrono e aspetta la ricezione
+     *         Ottieni la chiave relativa al valore
+     *         Testa la chiave
+     *         Se la chiave è stata trovata in questo processo:
+     *             Invia un bcast
+     *             Attendi che gli altri processi siano pronti a ricevere
      *         Altrimenti:
-     *           Setta il flag "chiave trovata"
-     *           Manda un bcast asincrono e aspetta la ricezione
+     *           Controlla se sia arrivato un messaggio asincrono
+     *           Se il messaggio è arrivato:
+     *             Invia un bcast
+     *             Attendi che gli altri processi siano pronti a ricevere
      *       Attendi la fine degli altri processi
      * Attendi la fine degli altri processi
      * Se il processo è quello che ha trovato la chiave:
@@ -69,10 +67,12 @@ int main(int argc, char **argv) {
 
     InputDESCrack input = inputDESCrackInit(argc, argv);
     DESBlock keyTemp = {0, 0};
-    int keyFoundFlag = 0, isReceived = 0, size = 0, rank = 0;
+    int size = 0, rank = 0, received = 0;
+    bool keyFound = false, keyFoundOutside = false;
     long pows[9] = {1}, lowerBound = 0, upperBound = 0;
     MPI_Request request;
 
+    // Inizio sezione MPI
     MPI_Init(NULL, NULL);
     // Imposto size e rank del processo
     MPI_Comm_size(MPI_COMM_WORLD, &size);
@@ -82,32 +82,27 @@ int main(int argc, char **argv) {
         pows[i] = pows[i - 1] * input.alphabetLength;
     // Se il processo non è "l'ultimo"...
     if (rank != size - 1) {
-        // Prepara la ricezione di un bcast asincrono dal processo con rank = size - 1
-        MPI_Ibcast(NULL, 0, MPI_INT, size - 1, MPI_COMM_WORLD, &request);
         // Se la chiave non è stata ancora trovata, ripeti per ogni valore della lunghezza della chiave...
-        for (int i = input.minKeyLength; i <= input.maxKeyLength && !keyFoundFlag; i++) {
+        for (int i = input.minKeyLength; i <= input.maxKeyLength && !keyFound && !keyFoundOutside; i++) {
             // Calcola l'intervallo di iterazione relativo al rank del processo
             setRangeMPI(size, rank, pows, i, &lowerBound, &upperBound);
             // Se la chiave non è stata ancora trovata, ripeti per ogni possibile valore...
-            for (long j = lowerBound; j < upperBound && !keyFoundFlag; j++) {
-                // Testa la ricezione del bcast
-                MPI_Test(&request, &isReceived, MPI_STATUS_IGNORE);
-                // Se il bcast non è arrivato...
-                if (!isReceived) {
-                    // Converti il valore nella chiave specifica
-                    keyTemp = valueToKey(i, j, pows, input.alphabet);
-                    // Testa la chiave
-                    keyFoundFlag = keyTest(&input.cipherTextBlock, &input.plainTextBlock, &keyTemp);
-                    if (keyFoundFlag) {
-                        // Manda un messaggio asincrono al processo con rank = size - 1
-                        MPI_Isend(NULL, 0, MPI_INT, size - 1, 0, MPI_COMM_WORLD, &request);
-                        // Aspetta la ricezione
-                        MPI_Wait(&request, MPI_STATUS_IGNORE);
-                    }
-                }
-                else
-                    keyFoundFlag = 1;
+            for (long j = lowerBound; j < upperBound && !keyFound && !keyFoundOutside; j++) {
+                // Converti il valore nella chiave specifica
+                keyTemp = valueToKey(i, j, pows, input.alphabet);
+                // Testa la chiave
+                keyFound = keyTest(&input.cipherTextBlock, &input.plainTextBlock, &keyTemp);
             }
+            // Se la chiave è stata trovata...
+            if (keyFound) {
+                // Manda un messaggio asincrono al processo con rank "size - 1" e aspetta la ricezione
+                MPI_Isend(NULL, 0, MPI_INT, size - 1, 0, MPI_COMM_WORLD, &request);
+                MPI_Wait(&request, MPI_STATUS_IGNORE);
+            }
+            // Attendi che il bcast sia inviato
+            MPI_Barrier(MPI_COMM_WORLD);
+            // Ricevi il bcast
+            MPI_Bcast(&keyFoundOutside, 1, MPI_C_BOOL, size - 1, MPI_COMM_WORLD);
             // Attendi il termine dell'iterazione degli altri processi
             MPI_Barrier(MPI_COMM_WORLD);
         }
@@ -115,33 +110,35 @@ int main(int argc, char **argv) {
     // ...altrimenti
     else {
         // Prepara la ricezione di un messaggio asincrono da un qualsiasi processo
-        MPI_Irecv(NULL, 0, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &request);
+        MPI_Irecv(NULL, 0, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &request);
         // Se la chiave non è stata ancora trovata, ripeti per ogni valore della lunghezza della chiave...
-        for (int i = input.minKeyLength; i <= input.maxKeyLength && !keyFoundFlag; i++) {
+        for (int i = input.minKeyLength; i <= input.maxKeyLength && !keyFound && !keyFoundOutside; i++) {
             // Calcola l'intervallo di iterazione relativo al rank del processo
             setRangeMPI(size, rank, pows, i, &lowerBound, &upperBound);
             // Se la chiave non è stata ancora trovata, ripeti per ogni possibile valore...
-            for (long j = lowerBound; j < upperBound && !keyFoundFlag; j++) {
-                // Testa la ricezione del messaggio
-                MPI_Test(&request, &isReceived, MPI_STATUS_IGNORE);
-                // Se il messaggio non è arrivato...
-                if (!isReceived) {
-                    // Converti il valore nella chiave specifica
-                    keyTemp = valueToKey(i, j, pows, input.alphabet);
-                    // Testa la chiave
-                    keyFoundFlag = keyTest(&input.cipherTextBlock, &input.plainTextBlock, &keyTemp);
-                    if (keyFoundFlag) {
-                        // Manda un bcast asincrono e aspetta la ricezione
-                        MPI_Ibcast(NULL, 0, MPI_INT, rank, MPI_COMM_WORLD, &request);
-                        MPI_Wait(&request, MPI_STATUS_IGNORE);
-                    }
-                }
-                else {
-                    keyFoundFlag = 1;
-                    // Manda un bcast asincrono e aspetta la ricezione
-                    MPI_Ibcast(NULL, 0, MPI_INT, rank, MPI_COMM_WORLD, &request);
-                    MPI_Wait(&request, MPI_STATUS_IGNORE);
-                }
+            for (long j = lowerBound; j < upperBound && !keyFound && !keyFoundOutside; j++) {
+                // Converti il valore nella chiave specifica
+                keyTemp = valueToKey(i, j, pows, input.alphabet);
+                // Testa la chiave
+                keyFound = keyTest(&input.cipherTextBlock, &input.plainTextBlock, &keyTemp);    
+            }
+            // Se la chiave è stata trovata...
+            if (keyFound) {
+                // Invia un bcast agli altri processi
+                MPI_Bcast(&keyFound, 1, MPI_C_BOOL, rank, MPI_COMM_WORLD);
+                // Attendi che gli altri processi siano pronti a leggere
+                MPI_Barrier(MPI_COMM_WORLD);
+            }
+            // ...altrimenti
+            else {
+                // Testa se il messaggio asincrono sia arrivato
+                MPI_Test(&request, &received, MPI_STATUS_IGNORE);
+                // Se il messaggio è arrivato...
+                keyFoundOutside = received == 1;
+                // Invia un bcast agli altri processi
+                MPI_Bcast(&keyFoundOutside, 1, MPI_C_BOOL, rank, MPI_COMM_WORLD);
+                // Attendi che gli altri processi siano pronti a leggere
+                MPI_Barrier(MPI_COMM_WORLD);
             }
             // Attendi il termine dell'iterazione degli altri processi
             MPI_Barrier(MPI_COMM_WORLD);
@@ -149,12 +146,13 @@ int main(int argc, char **argv) {
     }
     // Attendi il termine dell'iterazione degli altri processi
     MPI_Barrier(MPI_COMM_WORLD);
+    // Fine sezione MPI
+    MPI_Finalize();
     // Se la chiave è stata trovata in questo processo...
-    if (keyFoundFlag && !isReceived) {
+    if (keyFound) {
         // ...forza la parità (dispari) e stampala
         forceOddParity(&keyTemp);
         printf("%08x%08x\n", keyTemp.hi, keyTemp.lo);
     }
-    MPI_Finalize();
     return EXIT_SUCCESS;
 }
